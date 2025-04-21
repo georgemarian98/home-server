27e97834d1d337e01ec14efd550cbcae38ce83dd
George <georgemarian98@gmail.com>
2025-04-21 22:06:21 +0100

    Add Homarr service

--- start_services.sh
+++ start_services.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-DOCKER_FILES=$(find -name "docker-compose.yaml")
-echo $DOCKER_FILES
+DOCKER_FILES=$(find -name "docker-compose.yaml" -type f)
+echo "Services: "
+echo "$DOCKER_FILES" | awk -F/ '{ printf "    %s\n", $2 }'
+
+SERVICE=""
+while getopts "f:" arg;
+do
+    case $arg in
+        f) SERVICE=$OPTARG ;;
+    esac
+done
+
+if [ ! -z "$SERVICE" ]; then
+    # use grep instead, it's more readable
+    for file in $DOCKER_FILES;
+    do
+        if [[ "$file" == *"$SERVICE"* ]];
+        then
+            DOCKER_FILES=$file
+            break
+        fi
+    done
+fi
+
+docker-compose --verbose -f $DOCKER_FILES up
