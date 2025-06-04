69b1b9e42b7dd027baff10b9b64bae4dbc69260b
George <georgemarian98@gmail.com>
2025-06-04 20:57:51 +0100

    Add option to the script to create a new service template

--- start_services.sh
+++ start_services.sh
@@ -2,47 +2,63 @@
 
 usage()
 {
-    printf "Usage: $0 -r -f <DOCKER_SERVICE_NAME>
+    printf "Usage: $0 -c <NEW_SERVICE_NAME> -r -f <DOCKER_SERVICE_NAME>
+    -c - Create a new folder with the README and the docker compose file for the new service
     -r - Remove the service(s)
     -f - Start/Remove just a specific service\n"
     exit 1
 }
 
+create_new_service()
+{
+    local SERVICE_NAME=$1
+    mkdir ./$SERVICE_NAME
+    printf "# ${SERVICE_NAME^}\n" > ./$SERVICE_NAME/README.md
+    printf "version: '3.7'\n" > ./$SERVICE_NAME/docker-compose.yaml
+    exit 0
+}
+
+main()
+{
+    local DOCKER_FILES=$(find -maxdepth 2 -name "docker-compose.yaml" -type f)
+
+    if [ ! -z "$SERVICE" ]; 
+    then
+        for file in $DOCKER_FILES;
+        do
+            if [[ "$file" == *"$SERVICE"* ]]; 
+            then
+                DOCKER_FILES=$file
+                break
+            fi
+        done
+    fi
+
+    echo "Services: "
+    for file in $DOCKER_FILES;
+    do
+        local SERVICE_NAME=$(basename $(dirname $file))
+        if [ ! -z "$REMOVE" ]; 
+        then
+            docker-compose -f $file --env-file ./.env -- down
+        else
+            docker-compose -f $file --env-file ./.env -- pull
+            docker-compose -f $file --env-file ./.env -- up -d
+        fi
+        echo "---------------------------------------------------"
+    done
+}
+
 SERVICE=""
 REMOVE=""
-while getopts "f:r" arg;
+while getopts "c:f:r" arg;
 do
     case $arg in
+        c) create_new_service $OPTARG ;;
         f) SERVICE=$OPTARG ;;
         r) REMOVE="true" ;;
         *) usage
     esac
 done
 
-DOCKER_FILES=$(find -maxdepth 2 -name "docker-compose.yaml" -type f)
-
-if [ ! -z "$SERVICE" ]; 
-then
-    for file in $DOCKER_FILES;
-    do
-        if [[ "$file" == *"$SERVICE"* ]]; 
-        then
-            DOCKER_FILES=$file
-            break
-        fi
-    done
-fi
-
-echo "Services: "
-for file in $DOCKER_FILES;
-do
-    SERVICE_NAME=$(basename $(dirname $file))
-    if [ ! -z "$REMOVE" ]; 
-    then
-        docker-compose -f $file --env-file ./.env -- down
-    else
-        docker-compose -f $file --env-file ./.env -- pull
-        docker-compose -f $file --env-file ./.env -- up -d
-    fi
-    echo "---------------------------------------------------"
-done
+main
