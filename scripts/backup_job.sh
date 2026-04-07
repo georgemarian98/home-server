76760a7f64d07e0e0b1ae3db5e2cff0ef91f1418
George <georgemarian98@gmail.com>
2026-04-07 23:47:33 +0300

    [Scripts] Finish script to automatically backup the server

--- scripts/backup_job.sh
+++ scripts/backup_job.sh
@@ -1,51 +1,80 @@
 #!/usr/bin/env bash
 
+CURRENT_DIRECTORY="$(dirname $(readlink -f "$0"))"
+
 # .env.backup file should contain the following variables:
-# PI_RESTIC_REPO=<user@host:path> # Restic repository for the Raspberry Pi backup
-# HDD_RESTIC_REPO=<user@host:path> # Restic repository for the extra HDD backup
-# PROXMOX_MAC_ADDRESS=<MAC_ADDRESS> # MAC address of the Proxmox server for wake-on-LAN
-# PROXMOX_IP=<IP_ADDRESS> # IP address of the Proxmox server for SSH access
-# GONDOLIN_IP=<IP_ADDRESS> # IP address of the Debian server for SSH access
-# DOCKER_SERVICE_CONFIG_NAS_PATH=<path> # Path to the docker_volumes folder on the NAS
-source ".env.backup"
+# PROXMOX_MAC_ADDRESS=<MAC_ADDRESS> - MAC address of the Proxmox server for wake-on-LAN
+# PROXMOX_IP=<IP_ADDRESS> - IP address of the Proxmox server for SSH access
+# PROXMOX_SSH_USER=<SSH_USER> - SSH user for the Proxmox server
+# DEBIAN_IP=<IP_ADDRESS> - IP address of the Debian server for SSH access
+# DISCORD_WEBHOOK_TOKEN=<TOKEN> - Webhook token for sending notifications to Discord
+source "$CURRENT_DIRECTORY/.env.backup"
 
-# wakeup proxmox server if it's powered off
-if ! ping -c 1 "$PROXMOX_IP" &> /dev/null; then
-    echo "Proxmox server is offline. Sending wake-on-LAN packet..."
-    wakeonlan "$PROXMOX_MAC_ADDRESS"
-    echo "Waiting for Proxmox server to wake up..."
-    while ! ping -c 1 "$PROXMOX_IP" &> /dev/null; do
-        sleep 5
+backup_server()
+{
+    # Wake up proxmox server if it's powered off
+    if ! ping -c 1 "$PROXMOX_IP" &> /dev/null; then
+        echo "Proxmox server is offline. Sending wake-on-LAN packet..."
+        wakeonlan "$PROXMOX_MAC_ADDRESS"
+        
+        COUNT=0
+        echo "Waiting for Proxmox server to wake up..."
+        while ! ping -c 1 "$PROXMOX_IP" &> /dev/null && ! ping -c 1 "$DEBIAN_IP" &> /dev/null; do
+            sleep 5
+            ((COUNT++))
+            if [[ $COUNT -ge 60 ]]; then
+                echo "Timeout waiting for servers."
+                exit 1
+            fi
+        done
+        echo "Proxmox server is online."
+    fi
+
+    while docker node ls 2>&1 | grep "Error response from daemon" > /dev/null; do
+        echo "Waiting for Gondolin server to fully boot up and the docker containers to start..."
+        sleep 60
     done
-    echo "Proxmox server is online."
-fi
+ 
+    BACKUP_LOG_DIR="$HOME/backup_logs"
+    ANSIBLE_LOG_FILE="$BACKUP_LOG_DIR/ansible_log_$(date +%Y_%m_%d).txt"
 
-# Stop all docker containers
-docker stop $(docker ps -a --format="{{.Names}}")
+    mkdir -p "$BACKUP_LOG_DIR"
 
-# rsync .docker_service_config folder to NAS
-echo "Syncing .docker_service_config to NAS..."
-rsync -aAXv --delete "$HOME/.docker_service_config/" "$DOCKER_SERVICE_CONFIG_NAS_PATH"
+    # Keep the ansible log file only on failure
+    echo "Starting backup process..."
+    cd "$CURRENT_DIRECTORY/../ansible"
+    if ansible-playbook backup.yaml > "$ANSIBLE_LOG_FILE" 2>&1; then
+        rm "$ANSIBLE_LOG_FILE"
+        echo "Backup completed successfully."
+    fi
 
-# Start the docker conainers again
-docker start $(docker ps -a --format="{{.Names}}")
+    # Shutdown the proxmox server after the backup is done
+    echo "Shutting down Proxmox server..."
+    ssh "$PROXMOX_SSH_USER@$PROXMOX_IP" "shutdown -P now"
+}
 
-# rsync .docker_service_config from the debian server to the NAS
-ssh "$GONDOLIN_IP" "docker stop $(docker ps -a --format=\"{{.Names}}\")"
-ssh "$GONDOLIN_IP" "sudo rsync -aAXv --delete  --exclude="jellyfin/" $HOME/.docker_service_config/ $DOCKER_SERVICE_CONFIG_NAS_PATH"
-ssh "$GONDOLIN_IP" "docker start $(docker ps -a --format=\"{{.Names}}\")"
+main()
+{
+    BEFORE_AVAILABLE_SPACE=$(du -sb "$HOME/usb_share/backup" | awk '{print $1}')
 
-TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-BACKUP_PI_LOG="$home/backup_log_$TIMESTAMP.txt"
+    backup_server
+    
+    AFTER_AVAILABLE_SPACE=$(du -sb  "$HOME/usb_share/backup" | awk '{print $1}')
 
-# run backup_folder.sh script on the debian server to backup the needed data back to pi
-ssh "$GONDOLIN_IP" "bash -s" < $HOME/home-server/scripts/backup_folder.sh "$PI_RESTIC_REPO" >> "$BACKUP_PI_LOG" 2>&1
+    # Generate stats file
+    STATS_FILE="${BACKUP_LOG_DIR}/STATS_$(date +%Y_%m_%d).txt"
+    DURATION="$((($(date +%s) - $(date -d "today 04:00:00" +%s))/60))min"
+    DATA_ADDED=$(echo $(( $AFTER_AVAILABLE_SPACE - $BEFORE_AVAILABLE_SPACE )) | numfmt --to=iec --suffix=B)
+    AVAILABLE_SPACE="$(df -h "$HOME/usb_share" | tail -1 | awk '{print $4}')B"
+    printf "Backup duration: ${DURATION}\nData added: ${DATA_ADDED}\nCurrent available space: ${AVAILABLE_SPACE}\n" > "${STATS_FILE}"
+    
+    # Send discord notification with the backup report
+    echo "Sending backup report to Discord..."
+    $CURRENT_DIRECTORY/send_discord_notification.sh -t "${DISCORD_WEBHOOK_TOKEN}" -b
 
-# run backup_folder.sh script on the debian server to backup again the needed data to the extra HDD
-#TODO
+    # Delete old backup logs
+    find "$BACKUP_LOG_DIR" -type f -name "backup_log_*.txt" -mtime +7 -delete
+    rm "$STATS_FILE"
+}
 
-# shutdown the proxmox server after the backup is done
-ssh "$PROXMOX_IP" "sudo shutdown -p now"
-
-# Example cron job to run the backup script every day at 4am
-# 0 4 * * * /home/george/home-server/scripts/backup_job.sh
+main
